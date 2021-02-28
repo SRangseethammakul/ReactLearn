fe2909288c5354c2c3b9ddd1e40eb4148dad9632
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-02-28 12:20:48 +0700

    Axios basic

--- src/pages/AboutePage.js
+++ src/pages/AboutePage.js
@@ -1,11 +1,32 @@
 import React from 'react'
-
+import axios from 'axios'
 const AboutePage = () => {
+    const [version, setVersion] = React.useState('')
+
+    const getData  = async () => {
+            const resp = await axios.get('https://api.codingthailand.com/api/version');
+            setVersion(resp.data.data.version);
+        }
+    React.useEffect(() => {
+        // async function getData() {
+        //     const resp = await axios.get('https://api.codingthailand.com/api/version');
+        //     setVersion(resp.data.data.version)
+        // }
+        getData();
+    },[])
     return (
         <div className="container">
             <div className="row mt-4">
                 <div className="col-md-12">
                     <h2>เกี่ยวกับเรา</h2>
+                    {
+                        version && (
+                            <p>
+                                Backend API VERSION : {version}
+                            </p>
+                        )
+                    }
+
                 </div>
             </div>
         </div>
