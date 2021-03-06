72cf27fa319eacb3c468d4fb4f18ec95f83996ae
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-03-06 23:38:02 +0700

    add package

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom"
 import Footer from './components/Footer'
 import HospitalPage from './components/Hospital/HospitalPage'
